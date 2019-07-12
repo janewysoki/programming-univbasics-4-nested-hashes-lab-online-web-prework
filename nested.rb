14a9fd47ec3c3571332a57a907c17cdb52319390
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-12 15:52:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,8 +108,8 @@ def changing_alan
         }
      }
      programmer_hash[:alan_kay] = {
-          :known_for => "GUI",
-          :languages => ["Smalltalk", "LISP"]
+          :known_for => "GUI"
+          
         }
      programmer_hash
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
